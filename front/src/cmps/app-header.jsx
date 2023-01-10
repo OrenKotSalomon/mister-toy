5f40df083fc4733e95beaf03310948f3da2202a5
OrenKotSalomon <orensalomonkot@gmail.com>
2023-01-10 13:05:08 +0200

    added pictures to toys

--- front/src/cmps/app-header.jsx
+++ front/src/cmps/app-header.jsx
@@ -2,14 +2,14 @@
 
 import { useEffect } from "react";
 import { Link, NavLink } from "react-router-dom";
-
+import images from '../assets/img/images.png'
 
 export function AppHeader() {
 
 
     return <header className="app-header">
         <Link className="logo" to="/">
-            <h3 >LOGO!</h3>
+            <img src={images} alt="" />
         </Link>
         <nav className="link-container">
             <NavLink to="/">Home</NavLink>
--- front/src/cmps/toy-preview.jsx
+++ front/src/cmps/toy-preview.jsx
@@ -12,18 +12,24 @@ export function ToyPreview({ toy, onDeleteToy }) {
     return (
         <div className="toy-container" >
             <h2 className="toy-name">{toy.name}</h2>
-            <div className="toy-name">{toy.description}</div>
-            <div className={`toy-name ${toy.price > 150 ? 'red' : 'green'}`} >Price {toy.price}$</div>
-            <div className="toy-name">created at: {utilService.time_ago(toy.createdAt)}</div>
-            <div className="toy-name">{toy.inStock ? 'In stock' : 'Out of stock'}</div>
-            <div className="label-container">
-                {toy.labels.map((label, idx) => <div key={idx}>{label}</div>)}
+            <div className="toy-img-container">
+                <img src={`https://robohash.org/${toy.name}.png?set=set1`} alt="" />
+            </div >
+            <div className="toy-container">
 
-            </div>
-            <div className="preview-link-container">
-                <button onClick={() => onDeleteToy(toy)}>delete</button>
-                <Link to={`/toy/edit/${toy._id}`}>edit</Link>
-                <Link to={`/toy/details/${toy._id}`}>details</Link>
+                <div className="toy-name">{toy.description}</div>
+                <div className={`toy-name ${toy.price > 150 ? 'red' : 'green'}`} >Price {toy.price}$</div>
+                <div className="toy-name">created at: {utilService.time_ago(toy.createdAt)}</div>
+                <div className="toy-name">{toy.inStock ? 'In stock' : 'Out of stock'}</div>
+                <div className="label-container">
+                    {toy.labels.map((label, idx) => <div key={idx}>{label}</div>)}
+
+                </div>
+                <div className="preview-link-container">
+                    <button onClick={() => onDeleteToy(toy)}>delete</button>
+                    <Link to={`/toy/edit/${toy._id}`}>edit</Link>
+                    <Link to={`/toy/details/${toy._id}`}>details</Link>
+                </div>
             </div>
 
         </div>
